d651bf9f59827ed27f54c232c39274f31f9ed83d
Mohammed Imran <mimran@Imran.local>
2016-03-13 08:10:26 +0530

    Getting a service with in the custom service

--- src/Services/CustomBuildServices.php
+++ src/Services/CustomBuildServices.php
@@ -2,8 +2,23 @@
 
 namespace Drupal\skeleton\Services;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
 class CustomBuildServices {
+
+
+    /**
+     * @var LoggerChannelFactoryInterface
+     */
+    private $loggerFactory;
+
+    public function __construct(LoggerChannelFactoryInterface $loggerFactory){
+        $this->loggerFactory = $loggerFactory;
+    }
+
     public function page_arguments(){
-        return 'hai';
+        $serviceMessage = 'This message is from custom service';
+        $this->loggerFactory->get('default')->debug($serviceMessage);
+        return $serviceMessage;
     }
 }
