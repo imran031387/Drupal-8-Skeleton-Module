9a9f119bcf1c5789ce9986b49f2d3865a6cc945a
Mohammed Imran <mimran@Imran.local>
2016-03-14 10:43:47 +0530

    Using Configuration in service file using parameters.

--- src/Services/CustomBuildServices.php
+++ src/Services/CustomBuildServices.php
@@ -11,14 +11,20 @@ class CustomBuildServices {
      * @var LoggerChannelFactoryInterface
      */
     private $loggerFactory;
+    private $errorLogging;
 
-    public function __construct(LoggerChannelFactoryInterface $loggerFactory){
+    public function __construct(LoggerChannelFactoryInterface $loggerFactory,$errorLogging){
         $this->loggerFactory = $loggerFactory;
+        $this->errorLogging = $errorLogging;
     }
 
     public function service_example(){
         $serviceMessage = 'This message is from custom service';
-        $this->loggerFactory->get('default')->debug($serviceMessage);
+        // Check for the errorlogging status from the configuration file.
+        if($this->errorLogging){
+            $this->loggerFactory->get('default')->debug($serviceMessage);
+        }
+
         return $serviceMessage;
     }
 }
